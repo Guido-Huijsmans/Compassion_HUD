7cacf6c5dfa0ed5dff18ec5d451fb822176b0f93
Guido Huijsmans <guidohuijsmans@gmail.com>
2026-05-06 23:18:11 +0200

    Initial commit

---
+++ src/main/kotlin/uberpookie/compassion/CompassionHUDDataGenerator.kt
@@ -0,0 +1,9 @@
+package uberpookie.compassion
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object CompassionHUDDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
